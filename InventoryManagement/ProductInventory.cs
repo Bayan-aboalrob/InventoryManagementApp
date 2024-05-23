c19d9e17f37161176f0d3cbfd6ca25e8c1138cd1
Bayan Rub <bayanaboalrob2001@gmail.com>
2024-05-23 12:48:23 +0300

    Implementing Inventory management system with products and productInventory classes

---
+++ InventoryManagement/ProductInventory.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    internal class ProductInventory
+    {
+        private List<Product> products;
+        public ProductInventory(List<Product> products)
+        {
+            this.products = products;
+        }
+        public List<Product> GetProducts()
+        {
+            return products;
+        }
+        public static void AddProduct(ProductInventory inventory)
+        {
+
+            Console.WriteLine("Please enter the product name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Please enter the product price:");
+            double price;
+            while (!double.TryParse(Console.ReadLine(), out price))
+            {
+                Console.WriteLine("Invalid price. Please enter a valid number:");
+            }
+
+            Console.WriteLine("Please enter the quantity of the product:");
+            int quantity;
+            while (!int.TryParse(Console.ReadLine(), out quantity))
+            {
+                Console.WriteLine("Invalid quantity. Please enter a valid number:");
+            }
+
+            Product P = inventory.GetProducts().Find(prod => prod.Name.ToLower() == name.ToLower());
+            if (P != null)
+            {
+                P.Quantity += quantity;
+                Console.WriteLine($"{P.Name} quantity is updated successfully");
+            }
+            else
+            {
+                inventory.products.Add(new Product(name, price, quantity));
+                Console.WriteLine("Product added successfully !");
+            }
+
+
+
+        }
+        //Edit method for viewing a product modification options
+
+        public static void EditMenu()
+        {
+            Console.WriteLine("Please choose one of the following option");
+            Console.WriteLine("******************************************");
+            Console.WriteLine("1. Edit Quantity");
+            Console.WriteLine("2. Edit Price");
+
+        }
+
+        // This a method for editing a product details based on the user choice: whether to edit
+        // the price or the quantity
+        public static void EditProduct(ProductInventory inventory)
+        {
+            Console.WriteLine("Please enter the name of the product you want to edit:");
+            string productName = Console.ReadLine();
+
+            Product product = inventory.GetProducts().Find(p => p.Name.ToLower() == productName.ToLower());
+
+            if (product != null)
+            {
+                EditMenu();
+                string editOption = Console.ReadLine();
+                int convertedEditOption;
+
+                if (int.TryParse(editOption, out convertedEditOption))
+                {
+                    switch (convertedEditOption)
+                    {
+                        case 1:
+                            Console.WriteLine("Please enter the new quantity of the product:");
+                            int newQuantity;
+                            while (!int.TryParse(Console.ReadLine(), out newQuantity))
+                            {
+                                Console.WriteLine("Invalid quantity. Please enter a valid number:");
+                            }
+                            product.Quantity = newQuantity;
+                            Console.WriteLine("Quantity updated successfully !");
+                            break;
+                        case 2:
+                            Console.WriteLine("Please enter the new price of the product:");
+                            double newPrice;
+                            while (!double.TryParse(Console.ReadLine(), out newPrice))
+                            {
+                                Console.WriteLine("Invalid price. Please enter a valid number:");
+                            }
+                            product.Price = newPrice;
+                            Console.WriteLine("Price updated successfully !");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Product not found.");
+            }
+        }
+        // View all product details:
+
+        public static void ViewInventory(ProductInventory inventory)
+        {
+            if (inventory == null)
+            {
+                Console.WriteLine("Inventory is null.");
+                return;
+            }
+
+            if (inventory.products.Count == 0)
+            {
+                Console.WriteLine("Inventory is empty.");
+                return;
+            }
+
+            Console.WriteLine("Inventory:");
+            foreach (Product p in inventory.products)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        // Delete a product Method
+
+        public static void DeleteProduct(ProductInventory inventory)
+        {
+            Console.WriteLine("Enter the name of the product to delete:");
+            string name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Invalid product name.");
+                return;
+            }
+            Product product = inventory.GetProducts().Find(p => p.Name.ToLower() == name.ToLower());
+            if (product != null)
+            {
+                inventory.products.Remove(product);
+                Console.WriteLine($"{product.Name} is deleted successfully");
+            }
+            else
+            {
+                Console.WriteLine("The product doesn't exist  in the inventory");
+            }
+
+        }
+
+
+        // This method is used for searching for a product in the inventory, if it exits it prints all its details
+        //  else it prints "this product doesn't exist in the inventory"
+        public static void SearchProductByName(ProductInventory inventory)
+        {
+            string name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Invalid product name.");
+                return;
+            }
+            Product product = inventory.GetProducts().Find(p => p.Name.ToLower() == name.ToLower());
+            if (product != null)
+            {
+                Console.WriteLine("Here is the info of the product you are searching about");
+                Console.WriteLine($"{product}");
+            }
+            else
+            {
+                Console.WriteLine("The product doesn't exist  in the inventory");
+            }
+        }
+
+    }
+
+}
--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -1,2 +1,99 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static InventoryManagement.ProductInventory;
+namespace InventoryManagement
+{
+    public class Program
+    {
+        public static void MainMenu()
+        {
+            Console.WriteLine("Please choose one of the following option");
+            Console.WriteLine("******************************************");
+            Console.WriteLine("1. Add A Product");
+            Console.WriteLine("2. View Inventory");
+            Console.WriteLine("3. Edit A Product");
+            Console.WriteLine("4. Delete A Product");
+            Console.WriteLine("5. Search For A Product");
+            Console.WriteLine("6. Exit");
+        }
+        public static void EditMenu()
+        {
+            Console.WriteLine("Please choose one of the following option");
+            Console.WriteLine("******************************************");
+            Console.WriteLine("1. Edit Quantity");
+            Console.WriteLine("2. Edit Price");
+
+        }
+
+        static void Main(string[] args)
+        {
+            // Instantiating Two Products
+            Product P1 = new Product("Chetos", 3, 7);
+            Console.WriteLine(P1);
+            Product P2 = new Product("Lays", 9, 7);
+            Console.WriteLine(P2);
+
+            // Instantiating A list of Poducts
+            List<Product> products = new List<Product>();
+            products.Add(P1);
+            products.Add(P2);
+
+            // Instantiating An Inventory
+            var Inventory = new ProductInventory(products);
+
+            Console.WriteLine("Welcome to Inventory Management System !");
+
+            String userInput;
+            int userOption = 0;
+
+
+            while (true)
+            {
+                // Viewing the main Menu
+                Program.MainMenu();
+                // Choosing An Option
+                userInput = Console.ReadLine();
+
+                // Converting the option to int
+                if (!int.TryParse(userInput, out userOption))
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    return;
+                }
+
+                switch (userOption)
+                {
+                    case 1:
+
+                        ProductInventory.AddProduct(Inventory);
+                        break;
+                    case 2:
+                        Console.WriteLine("View Inventory");
+                        ProductInventory.ViewInventory(Inventory);
+                        break;
+                    case 3:
+                        ProductInventory.EditProduct(Inventory);
+                        Console.WriteLine("Edit Inventory");
+
+                        break;
+                    case 4:
+                        Console.WriteLine("Delete A Product");
+                        ProductInventory.DeleteProduct(Inventory);
+                        break;
+                    case 5:
+                        Console.WriteLine("Search For A Product");
+                        ProductInventory.SearchProductByName(Inventory);
+                        break;
+                    case 6:
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Please choose a number between 1 and 6.");
+                        break;
+                }
+            }
+
+
+        }
+
+    }
+}
